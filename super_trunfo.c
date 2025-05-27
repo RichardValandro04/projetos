62e308b395e9c8e5e96bf586852af6a9b6974bf4
Ríchard Valandro <118376541+RichardValandro04@users.noreply.github.com>
2025-05-27 10:18:07 +0000

    nivel mestre concluido

--- super_trunfo.c
+++ super_trunfo.c
@@ -31,7 +31,7 @@ int main(){
     nome_cidade_1[strcspn(nome_cidade_1, "\n")] = 0;
 
     printf("\nDigite a população da cidade: ");
-    scanf("%d", &populacao_1);
+    scanf("%lu", &populacao_1);
 
     printf("\nDigite a área da cidade em km²: ");
     scanf("%f", &area_1);
@@ -63,7 +63,7 @@ int main(){
     nome_cidade_2[strcspn(nome_cidade_2, "\n")] = 0;
 
     printf("\nDigite a população da cidade: ");
-    scanf("%d", &populacao_2);
+    scanf("%lu", &populacao_2);
 
     printf("\nDigite a área da cidade em km²: ");
     scanf("%f", &area_2);
@@ -78,11 +78,11 @@ int main(){
     
 
     //Calculando Densidade e Pib PerCapta
-    densidade_1 = populacao_1 / area_1;
-    pib_pc_1 = (pib_1 * 1000000000) / populacao_1; //Multiplicando por 1bi para obter o valor inteiro do pib
+    densidade_1 = (float)populacao_1 / area_1;
+    pib_pc_1 = (pib_1 * 1e9) / populacao_1; //Multiplicando por 1bi para obter o valor inteiro do pib
     
-    densidade_2 = populacao_2 / area_2;
-    pib_pc_2 = (pib_2 * 1000000000) / populacao_2; //Multiplicando por 1bi para obter o valor inteiro do pib
+    densidade_2 = (float)populacao_2 / area_2;
+    pib_pc_2 = (pib_2 * 1e9) / populacao_2; //Multiplicando por 1bi para obter o valor inteiro do pib
 
     
     //Calculando Densidade Inversa e Super Poder
@@ -99,12 +99,12 @@ int main(){
     printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
     
     //Exibindo informações primeira carta
-    printf("CARTA 1:\nEstado: %c \nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPib per Capta: %.2f reais\n", estado_1, codigo_1, nome_cidade_1, populacao_1, area_1, pib_1, pontos_turisticos_1, densidade_1, pib_pc_1);
+    printf("CARTA 1:\nEstado: %c \nCódigo: %s\nNome da Cidade: %s\nPopulação: %lu\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPib per Capta: %.2f reais\n", estado_1, codigo_1, nome_cidade_1, populacao_1, area_1, pib_1, pontos_turisticos_1, densidade_1, pib_pc_1);
 
     printf("----------------------------------------------------------------------------\n");
 
     //Exibindo informações segunda carta
-    printf("CARTA 2:\nEstado: %c \nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPib per Capta: %.2f reais \n", estado_2, codigo_2, nome_cidade_2, populacao_2, area_2, pib_2, pontos_turisticos_2, densidade_2, pib_pc_2);
+    printf("CARTA 2:\nEstado: %c \nCódigo: %s\nNome da Cidade: %s\nPopulação: %lu\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPib per Capta: %.2f reais\n", estado_2, codigo_2, nome_cidade_2, populacao_2, area_2, pib_2, pontos_turisticos_2, densidade_2, pib_pc_2);
 
 
     //Comparação de Cartas
